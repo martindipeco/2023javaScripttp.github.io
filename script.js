453552ee90a3e398c7016784e2eb16c4fcde924b
martindipeco <martindipeco@gmail.com>
2023-10-20 09:07:25 -0300

    agregado el switch de descuentos y el boton borrar

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
     var cantidadTicketsInput = document.getElementById("amount");
     var totalInput = document.getElementById("total");
     var botonResumen = document.getElementById("summary");
+    var botonBorrar = document.getElementById("delete");
+    var categoriaSelect = document.getElementById("category");
 
     botonResumen.addEventListener("click", function () {
         
@@ -9,14 +11,35 @@ document.addEventListener("DOMContentLoaded", function () {
         if (cantidadTickets > 0 &&  !isNaN(cantidadTickets))
         {
             var precio = 200; // Falta aplicar descuentos
-            var total = cantidadTickets * precio;
+            var descuento; 
+            var categoria = categoriaSelect.value;
+            switch (categoria)
+            {
+                case "estudiante":
+                    descuento = 0.2;
+                    break;
+                case "trainee":
+                    descuento = 0.5;
+                    break;
+                case "junior":
+                    descuento = 0.85;
+                    break;
+                default:
+                    descuento = 1; 
+            }
+            var total = cantidadTickets * precio * descuento;
             totalInput.value = total;
         } 
         else {
             alert("Por favor, ingresa una cantidad correcta.");
         }
     });
+
+    botonBorrar.addEventListener("click", function(){
+        cantidadTicketsInput.value = '';
+        totalInput.value = '';
+        categoriaSelect.value = "sinDescuento";
+    })
 });
 
 //FALTA: Boton BORRAR
-//FALTA: Aplicar Descuentos
